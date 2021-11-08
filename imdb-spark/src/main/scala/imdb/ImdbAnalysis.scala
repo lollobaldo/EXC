8e43cfb9e4f6d05446a1144c90f2275ec55c25f6
Lorenzo Baldini <lollobaldo2000@gmail.com>
2021-11-08 23:15:11 +0000

    cw2

--- imdb-spark/src/main/scala/imdb/ImdbAnalysis.scala
+++ imdb-spark/src/main/scala/imdb/ImdbAnalysis.scala
@@ -17,36 +17,82 @@ case class NameBasics(nconst: String, primaryName: Option[String], birthYear: Op
 
 object ImdbAnalysis {
 
-  val conf: SparkConf = ???
-  val sc: SparkContext = ???
+  val conf: SparkConf = new SparkConf().setAppName("imdb-spark").setMaster("local[1]")
+  val sc: SparkContext = new SparkContext(conf)
 
   // Hint: use a combination of `ImdbData.titleBasicsPath` and `ImdbData.parseTitleBasics`
-  val titleBasicsRDD: RDD[TitleBasics] = ???
+  val titleBasicsRDD: RDD[TitleBasics] = sc.textFile(ImdbData.titleBasicsPath).map(ImdbData.parseTitleBasics)
 
   // Hint: use a combination of `ImdbData.titleRatingsPath` and `ImdbData.parseTitleRatings`
-  val titleRatingsRDD: RDD[TitleRatings] = ???
+  val titleRatingsRDD: RDD[TitleRatings] = sc.textFile(ImdbData.titleRatingsPath).map(ImdbData.parseTitleRatings)
 
   // Hint: use a combination of `ImdbData.titleCrewPath` and `ImdbData.parseTitleCrew`
-  val titleCrewRDD: RDD[TitleCrew] = ???
+  val titleCrewRDD: RDD[TitleCrew] = sc.textFile(ImdbData.titleCrewPath).map(ImdbData.parseTitleCrew)
 
   // Hint: use a combination of `ImdbData.nameBasicsPath` and `ImdbData.parseNameBasics`
-  val nameBasicsRDD: RDD[NameBasics] = ???
+  val nameBasicsRDD: RDD[NameBasics] = sc.textFile(ImdbData.nameBasicsPath).map(ImdbData.parseNameBasics)
 
+  //  Calculate the average, minimum, and maximum runtime duration for all titles per movie genre.
+  //    Note that a title can have more than one genre, thus it should be considered for all of them. The results
+  //    should be kept in minutes and titles with 0 runtime duration are valid and should be accounted for in your
+  //    solution.
   def task1(rdd: RDD[TitleBasics]): RDD[(Float, Int, Int, String)] = {
-    ???
+    rdd
+      .filter(x => x.runtimeMinutes.isDefined && x.genres.isDefined)
+      .flatMap(x => x.genres.get.map((_, x.runtimeMinutes.get)))
+      .groupBy(_._1)
+      .map{case (genre, rs) => {
+        val (sum, tot, mi, ma) = rs.foldLeft((0, 0, Int.MaxValue, 0))((t: (Int, Int, Int, Int), r: (_, Int)) =>
+          (t._1 + r._2, t._2 + 1, r._2.min(t._3), r._2.max(t._4)))
+        ((sum.toFloat / tot), mi, ma, genre)
+      }}
   }
 
+  //  Return the titles of the movies which were released between 1990 and 2018 (inclusive), have an average
+  //  rating of 7.5 or more, and have received 500000 votes or more.
+  //    For the titles use the primaryTitle field and account only for entries whose titleType is ‘movie’.
   def task2(l1: RDD[TitleBasics], l2: RDD[TitleRatings]): RDD[String] = {
-    ???
+    val topRatings = l2
+      .filter(x => x.averageRating >= 7.5 && x.numVotes >= 500000)
+      .map(x => (x.tconst, ""))
+      .collectAsMap()
+    l1.filter(x =>
+      x.startYear.isDefined && x.primaryTitle.isDefined && x.titleType.isDefined
+        && x.startYear.get >= 1990 && x.startYear.get <= 2018 && x.titleType.get == "movie"
+        && topRatings.contains(x.tconst))
+      .map(_.primaryTitle.get)
   }
 
+  //  Return the top rated movie of each genre for each decade between 1900 and 1999.
+  //  For the titles use the primaryTitle field and account only for entries whose titleType is ‘movie’. For
+  //  calculating the top rated movies use the averageRating field and for the release year use the startYear
+  //    field.
+  //      The output should be sorted by decade and then by genre. For the movies with the same rating and of
+  //  the same decade, print only the one with the title that comes first alphabetically. Each decade should be
+  //    represented with a single digit, starting with 0 corresponding to 1900-1909.
   def task3(l1: RDD[TitleBasics], l2: RDD[TitleRatings]): RDD[(Int, String, String)] = {
-    ???
+    val ratingMap = l2.map(x => (x.tconst, x.averageRating)).collectAsMap();
+    l1
+      .filter(x => x.startYear.isDefined && x.genres.isDefined && x.startYear.get >= 1900 && x.startYear.get <= 1999
+        && x.primaryTitle.isDefined && x.titleType.isDefined && x.titleType.get == "movie" && ratingMap.contains(x.tconst))
+      .flatMap(x => x.genres.get.map((g => (ratingMap(x.tconst), x.startYear.get, g, x.primaryTitle.get))))
+      .groupBy(x => ((x._2/ 10) % 10, x._3))
+      .map{ case (key, value) => (key._1, key._2, value.minBy(y => (-y._1, y._4))._4) }
+      .sortBy(x => x)
   }
 
-  // Hint: There could be an input RDD that you do not really need in your implementation.
+  //  In this task we are interested in all the crew names (primaryName) for whom there are at least two knownfor
+  //    films released since the year 2010 up to and including the current year (2021).
+  //  You need to return the crew name and the number of such films.
   def task4(l1: RDD[TitleBasics], l2: RDD[TitleCrew], l3: RDD[NameBasics]): RDD[(String, Int)] = {
-    ???
+    val newMovies = l1
+      .filter(x => x.startYear.isDefined && x.startYear.get >= 2010 && x.startYear.get <= 2021)
+      .map(x => (x.tconst, ""))
+      .collectAsMap()
+    l3
+      .filter(x => x.primaryName.isDefined && x.knownForTitles.isDefined
+        && x.knownForTitles.get.count(y => newMovies.contains(y)) >= 2)
+      .map(x => (x.primaryName.get, x.knownForTitles.get.count(y => newMovies.contains(y))))
   }
 
   def main(args: Array[String]) {
